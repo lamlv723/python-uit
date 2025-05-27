a5f3cd3ea8dcfd100f8684874a4ed2b597ead405
Lam <lamlv723@gmail.com>
2025-05-28 00:12:35 +0700

    register model to admin

--- bike_stores/sales/admin.py
+++ bike_stores/sales/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Store, Staff, Order, OrderItem
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Store)
+admin.site.register(Staff)
+admin.site.register(Order)
+admin.site.register(OrderItem)
