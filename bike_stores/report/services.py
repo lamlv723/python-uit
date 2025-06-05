0e44b37630baf985b4fea39815aaf33ff831e919
Lam <lamlv723@gmail.com>
2025-06-06 00:59:10 +0700

    done 1

--- bike_stores/report/services.py
+++ bike_stores/report/services.py
@@ -6,26 +6,24 @@ def get_inventory_report_data()->dict:
     """
     Lấy dữ liệu tồn kho theo từng sản phẩm theo cửa hàng
     """
-
     inventory_items = (Stock.objects
-                       .select_related('products', 'stores')
-                       .order_by('store_name', 'product_name')
+                       .select_related('product_id', 'store_id')
+                       .order_by('store_id__store_name', 'product_id__product_name')
                     )
 
     report_data_grouped = {}
     for item in inventory_items:
-        store_name = item.store.store_name if item.store else "Không xác định"
+        store_name = item.store_id.store_name if item.store_id else "Không xác định"
 
-        product_name = item.product.product_name if item.product else "Không xác định"
+        product_name = item.product_id.product_name if item.product_id else "Không xác định"
 
         if store_name not in report_data_grouped:
             report_data_grouped[store_name] = []
 
         report_data_grouped[store_name].append({
-            'product_id': item.product.product_id if item.product else None,
+            'product_id': item.product_id.product_id if item.product_id else None,
             'product_name': product_name,
             'quantity': item.quantity,
-            # 'last_updated': item.last_updated # Nếu model Stock của bạn có trường này
         })
     print(inventory_items)
     return report_data_grouped
